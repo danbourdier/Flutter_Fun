864a832d7f12a273b1fbc8d0c0b8f57ac6ba0bf2
danbourdier <dfbourdier@gmail.com>
2021-04-15 13:57:38 -0400

    added build into MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+      home: Text('Test widget'),
+    );
+  }
+}
