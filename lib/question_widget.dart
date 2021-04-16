8db73dc4a3911e99ab3488c199f5b7bc5a9fca20
danbourdier <dfbourdier@gmail.com>
2021-04-16 17:13:45 -0400

    created question widget

---
+++ lib/question_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestionWidget extends StatelessWidget {
+  final String questionText;
+
+  QuestionWidget(this.questionText)
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("${this.questionText}");
+  }
+}
